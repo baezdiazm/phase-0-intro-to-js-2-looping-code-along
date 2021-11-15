6a2966333bcfbc3b68b76ec79cfd6d6f4c1106e2
baezdiazm <baezdiazm@yahoo.com>
2021-11-15 02:10:37 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+
+function countDown(time) {
+    while (time > -1) {
+        console.log(time)
+        time--
+    }
+}
